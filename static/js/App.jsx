7f0b508f2295cd71eae8c8e50c1f7a71bb9589dc
Fatima <fatima.hammouri@gmail.com>
2021-07-21 08:17:19 +0000

    added App component

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -6,3 +6,27 @@ const Route = ReactRouterDOM.Route;
 const Link = ReactRouterDOM.Link;
 const Switch = ReactRouterDOM.Switch;
 const useHistory = ReactRouterDOM.useHistory;
+
+function App() {
+    /* App component control Navigation among the App */
+
+    return (
+        <React.Fragment>
+            <Homepage />
+            <Router>
+
+                <Link to="/">Home</Link>
+
+                <Switch>
+
+                    <Route exact path="/">
+                        <Homepage />
+                    </Route>
+
+                </Switch>
+
+            </Router>
+
+        </React.Fragment>
+    )
+}
