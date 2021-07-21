9469349b86d8412f8bbc550b6f7c1c3340ea28b1
Fatima <fatima.hammouri@gmail.com>
2021-07-21 07:58:32 +0000

    import React Browser Router

---
+++ static/js/App.jsx
@@ -0,0 +1,8 @@
+"use strict";
+
+// import the BrowserRouter Components
+const Router = ReactRouterDOM.BrowserRouter;
+const Route = ReactRouterDOM.Route;
+const Link = ReactRouterDOM.Link;
+const Switch = ReactRouterDOM.Switch;
+const useHistory = ReactRouterDOM.useHistory;
