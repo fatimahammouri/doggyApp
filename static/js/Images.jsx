736bd92ce42cc752a726a244fee43a2a6381c325
Fatima <fatima.hammouri@gmail.com>
2021-07-26 11:20:30 +0000

    added comments/TODO

--- static/js/Images.jsx
+++ static/js/Images.jsx
@@ -1,24 +1,32 @@
 function Images() {
     /* Images component renders on a seperate page */
 
-    // useEffect to send a fetch request to the dog API images Endpoint
-    // on successful response from API, convert JSON response to
-    // js object then update the images state with value of the key "message"
-
-    // use useState Hook to save state of the images data
     const [images, setImages] = React.useState([])
-
-    // useParams saves the parameter from url (specific breed) so we can 
-    //  use it in fetching images for that specific breed
     const params = ReactRouterDOM.useParams();
+    /* useParams saves the parameter from url (specific breed) so we can 
+        use it in fetching images for that specific breed
+    */
     // console.log(params)
 
+
+    /* useEffect to send a fetch request to the dog API images Endpoint
+        on successful response from API, convert JSON response to
+        js object then update the images state with value of the key "message"
+    */
     React.useEffect(() => {
         fetch(`https://dog.ceo/api/breed/${params.id}/images`)
             .then(response => response.json())
             .then((imagesData) => setImages(imagesData.message))
     }, [])
+
+    /* for now we will show only Four images from the images Array
+    TODO:
+        add functionlity/option that allows user to load more images on demand
+        or specify the number of images to view
+    */
     let showImages = images.slice(0,4)
+
+    
     return (
         <div>
             <h1> {params.id} </h1>
