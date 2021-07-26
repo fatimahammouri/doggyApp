7722c20898526e66c7d68dd513d1d9ebf44e8f2a
Fatima <fatima.hammouri@gmail.com>
2021-07-26 11:50:45 +0000

    added comments/TODO

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
-""" Server with Pythons Flask Framework """
+""" Server with Pythons Flask Framework 
+TODO: this assumes that all routes will be rendered/handled within React fetch 
+    instead it's probably a better idea to send the GET/ POST requests to the API from Flasks
+    routes 
+    Make Backend with JavaScript as well by Installing Node and NPM        
+"""
 
 from flask import Flask, render_template
 app = Flask(__name__)
